33eb4187c88c0f6552fca9e9a0043b7b8d1be665
Ogi Martinovic <ogimart@gmail.com>
2023-02-27 07:28:24 -0500

    change font

--- emacs/.config/emacs/early-init.el
+++ emacs/.config/emacs/early-init.el
@@ -18,14 +18,14 @@
     (menu-bar-mode 0))
 (setq default-frame-alist '((width . 160) (height . 42)))
 (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
-(add-to-list 'default-frame-alist '(ns-appearance . light))
+(add-to-list 'default-frame-alist '(ns-appearance . dark))
 (setq ns-use-proxy-icon nil
       ns-antialias t
       ns-use-thin-smoothing nil)
 (pixel-scroll-precision-mode)
 ;; (setq x-underline-at-descent-line t)
 
-(set-face-attribute 'default nil :family "MonoLisa"
-		    :height 130 :weight 'normal)
+(set-face-attribute 'default nil :family "Source Code Pro"
+		    :height 150 :weight 'normal)
 (set-face-attribute 'variable-pitch nil :family "Helvetica Neue"
 		    :height 160 :weight 'normal)
